02140d7794f8dd37575968d1203e442309cfbf61
Karlito14 <leiroz26@hotmail.com>
2022-12-20 08:33:11 +0100

    Exercice 6 de la section facile

--- 00-facile/exercice-6/exercice-6.js
+++ 00-facile/exercice-6/exercice-6.js
@@ -1,7 +1,28 @@
 function exercice6() {
+    let phrase = "" ;
     for(let i = 1; i <= 200; i++){
-        if(i / 3)
+        if(Number.isInteger(i/3)){
+            if(Number.isInteger(i/15)){
+                phrase = phrase + "FizzBuzz - ";
+            } else {
+                phrase = phrase + "Fizz - ";
+            } 
+        } else if(Number.isInteger(i/5)){
+            if(Number.isInteger(i/15)){
+                phrase = phrase + "FizzBuzz - ";
+            } else {
+                if(i === 200){
+                    phrase = phrase + "Buzz";
+                } else{
+                    phrase = phrase + "Buzz - ";
+                }
+            }
+          } else {
+            phrase = phrase + i + " - ";
+        } 
     }
+    
+    return phrase;
 }
 
 
