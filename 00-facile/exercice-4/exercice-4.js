0d8962d3ae58bfdf224e4a3e139939e34b0ae5e9
Karlito14 <leiroz26@hotmail.com>
2023-02-23 17:40:07 +0100

    Exercice 0 niveau moyen

--- 00-facile/exercice-4/exercice-4.js
+++ 00-facile/exercice-4/exercice-4.js
@@ -1,5 +1,15 @@
 function exercice4(nombre) {
-  
+  const lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+  let chiffreRomain = '';
+
+  for (let i in lookup ) {
+    while ( nombre >= lookup[i] ) {
+        chiffreRomain += i;
+        nombre -= lookup[i];
+    }
+  }
+  return chiffreRomain;
+    
 }
 
 // Ne touchez pas à la ligne suivante
--- 01-moyen/exercice-0/exercice-0.js
+++ 01-moyen/exercice-0/exercice-0.js
@@ -1,5 +1,15 @@
 function exercice0(phrase) {
+  let newPhrase = "";
+  const tableau = phrase.split(' ');
   
+  for(let i = 0; i < tableau.length; i++){
+    if(tableau[i][0] === "\""){
+        newPhrase += tableau[i][1];
+    } else {
+        newPhrase += tableau[i][0];
+    }
+  }
+  return newPhrase.toUpperCase()
 }
 
 // Ne touchez pas à la ligne suivante
